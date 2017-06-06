1529c18615bbbfa758780ee3c427023660fa1099
Jeff Charlesworth <jcharlesworth02@gmail.com>
2017-06-06 15:48:06 -0700

    removed and refactored

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    if session[:user_id] then User.find(session[user_id]) end
+    User.find(session[user_id]) if session [:user_id]
   end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :first_name, :last_name
-
-  def email
-    object.email
-  end
 end
